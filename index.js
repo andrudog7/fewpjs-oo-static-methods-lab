0b851bd21297173fc950166084b247663b85fbfe
--add <andrewksmoker@gmail.com>
2021-02-02 14:20:33 -0500

    commit tree f8ad0a28c0c1902dbacff421146cacbcb6d5fd9a

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 class Formatter {
   //add static methods here
-}
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1)
+  }
+
+  static sanitize(str) {
+    return str.replace( /[^A-Za-z0-9 '-]/g, '' )
+  }
+
+  static titleize(str) {
+    let exceptions = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+    let newSentance = []
+    let words = str.split(" ")
+    // newSentance.push(words[0].charAt(0).toUpperCase() + words[0].slice(1))
+    words.forEach(function(word) {
+      if (words[0] === word) { 
+        newSentance.push(word.charAt(0).toUpperCase() + word.slice(1))
+      }
+      else {
+            if (exceptions.find(element => element === word)) {
+              newSentance.push(word)
+            } else {
+              newSentance.push(word.charAt(0).toUpperCase() + word.slice(1)) 
+            }
+          }
+          })
+        return newSentance.join(" ")}
+    }
